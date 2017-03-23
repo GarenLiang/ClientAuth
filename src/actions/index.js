c2d77662cda8413ec50b1ba81461a538e0c08c6a
GarenLiang <ideabreeze@gmail.com>
2017-03-22 22:25:09 -0700

    State Update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,24 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
     //submit email and password to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-    //if request is good
-    //update state
-    //save jwt token
-    //redirect route
-
-    //if request is bad
-    //show an error to user
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        //if request is good
+        //update state
+        dispatch({ type: AUTH_USER });
+        //save jwt token
+        //redirect route
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        //if request is bad
+        //show an error to user
+      });
   }
 }
