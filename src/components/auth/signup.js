c53810f1d6b238388a8371fb0f72f54987fd6131
GarenLiang <ideabreeze@gmail.com>
2017-03-23 21:41:03 -0700

    Validation

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -10,19 +10,41 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Email:</label>
           <input className="form-control" {...email} />
+          { email.touched && email.error && <div className="error">{email.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
           <input className="form-control" {...password} type="password" />
+          {password.touched && password.error && <div className="error">{password.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Confirm Password:</label>
           <input className="form-control" {...passwordConfirm} type="password" />
+          {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
         <button action="submit" className="btn btn-primary right" id='1'>Sign Up!</button>
       </form>
     );
   }
+}
+function validate(formProps) {
+  const errors = {};
+
+  if (!formProps.email) {
+    errors.email = 'Please enter the email';
+  }
+
+  if (!formProps.password) {
+    errors.password = 'Please enter a password';
+  }
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = 'Please enter the password confirmation';
+  }
+
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Password must match';
+  }
+  return errors;
 }
 export default reduxForm({
   form: 'signup',
